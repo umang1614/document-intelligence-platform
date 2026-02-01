2e9c1b896aa559b266acb7752afae68aa6fbc4d5
umangeval <umang.bhatia@evalueserve.com>
2026-02-01 17:40:37 +0530

    Phase 2 Done

--- backend/src/main/java/com/umang/docplatform/test/TestController.java
+++ backend/src/main/java/com/umang/docplatform/test/TestController.java
@@ -1,5 +1,6 @@
 package com.umang.docplatform.test;
 
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,6 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class TestController {
     
     @GetMapping("/secure-test")
+    @PreAuthorize("hasRole('ADMIN')")
     public String secureTest() {
         return "You are authenticated";
     }
